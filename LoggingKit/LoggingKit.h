8c305e6a0b43328168472bef114afc0da7472a53
林達也 <t_hayashi@hayashitatsuya-no-Mac-Pro.local>
2015-02-26 01:14:04 +0900

    Initial Commit

---
+++ LoggingKit/LoggingKit.h
@@ -0,0 +1,19 @@
+//
+//  LoggingKit.h
+//  LoggingKit
+//
+//  Created by 林達也 on 2015/02/26.
+//  Copyright (c) 2015年 林達也. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LoggingKit.
+FOUNDATION_EXPORT double LoggingKitVersionNumber;
+
+//! Project version string for LoggingKit.
+FOUNDATION_EXPORT const unsigned char LoggingKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LoggingKit/PublicHeader.h>
+
+
